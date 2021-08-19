1223e4bd8291d07de7db0a55e790f81443021072
shincloud <shincloudiddev@gmail.com>
2021-08-19 10:45:54 +0900

    Create talk_ai.py

---
+++ talk_ai.py
@@ -0,0 +1,22 @@
+import pya3rt
+from flask import Flask
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route("/")
+def hello():
+    apikey = "" #自分のAPIキーを設定する
+    client = pya3rt.TalkClient(apikey)
+    return "ようこそ"
+
+def talk_ai():
+  words = input("あなた >")
+  while words!="":
+    response = client.talk(words)
+    print("Bot >"+((response['results'])[0])['reply'])
+    words = input("あなた >")
+
+if __name__ == '__main__':
+    app.run()
